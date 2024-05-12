f6b7e77952c2deb02d0394097dc5d766c92b8168
KOREA-AICS <165271322+KOREA-AICS@users.noreply.github.com>
2024-05-12 19:30:20 +0900

    Update Permutation.c

--- Permutation.c
+++ Permutation.c
@@ -5,40 +5,52 @@
 #include <stdlib.h>
 #include <string.h>
 
-void Enc(char[], int, int);
+void Enc(char[], int, int, int);
 
 int main(void) {
 
 	char userInput[100];
 	int key;
 	int len;
+	int size;
 
-	printf("Input Plain Text : \n");
-	scanf(" %[^\n]s", userInput);
+	printf("##################################################\n\n");
 
-	len = strlen(userInput);
+	while (1) {
 
-	printf("Input seed : \n");
-	scanf(" %d", &key);
+		printf("Input Plain Text : \n");
+		scanf(" %[^\n]s", userInput);
 
-	printf("Encrypted Text : \n");
-	Enc(userInput, key, len);
+		len = strlen(userInput);
+
+		printf("Input seed : \n");
+		scanf(" %d", &key);
+
+		printf("Input slice size : \n");
+		scanf(" %d", &size);
+
+		printf("Encrypted Text : \n");
+		Enc(userInput, key, len, size);
+
+		printf("\n\n##################################################\n\n");
+	}
 
 
 	return 0;
 }
 
 
-void Enc(char plain[], int key, int len) {
+void Enc(char plain[], int key, int len, int size) {
 	int i = 0;
 	int j = 0;
+	int k = 0;
 	int a[100];
 	char encrypted[100];
 
 	srand(key);
 
-	for (i = 0; i < len; i++) {
-		a[i] = (rand() % len);
+	for (i = 0; i < size; i++) {
+		a[i] = (rand() % size);
 		for (j = 0; j < i; j++) {
 			if (a[i] == a[j]) {
 				i--;
@@ -48,8 +60,15 @@ void Enc(char plain[], int key, int len) {
 
 
 	for (i = 0; i < len; i++) {
-		encrypted[i] = plain[a[i]];
+		if (i != 0 && i% size == 0) {
+			k++;
+		}
 
+		encrypted[i] = plain[size*k+a[i%size]];
+
+		if (encrypted[i] == '?') {
+			encrypted[i] = ' ';
+		}
 		printf("%c", encrypted[i]);
 
 	}
